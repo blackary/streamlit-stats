b39a7eb09ec0c0f02aff5352f376a0e2d430da50
“Nathan <42682122+wxman22@users.noreply.github.com>
2021-07-07 17:28:14 -0400

    Reorganized regression script

--- advanced_regression/regression-assumptions-nl.py
+++ advanced_regression/regression-assumptions-nl.py
@@ -3,18 +3,20 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from statsmodels.stats.outliers_influence import variance_inflation_factor
 
 st.write(f"# Regression Assumptions")
 """
-Dangers: Coefficients can be biased.  Standard errors could be unreliable (then so too would be t-stat and p-value).
+Dangers: Coefficients can be biased.  Standard errors could be unreliable (then so too would be
+t-statistic and p-value).
 """
 """ 
 1. Linearity
-    - The regression needs to be linear in the parameters. You can have x^2 terms to represent squared data, so
-    the relationship is linear with y (and additive).
+    - The regression needs to be linear in the parameters. You can have x^2 terms to represent
+    squared data, so the relationship is linear with y (and additive).
 2. Constant Error Variance
-    - If you map out the distances between points and black fitted line, the variance scales with the values of the data.
-    We don't want Heteroskedasticity.
+    - If you map out the distances between points and black fitted line, the variance scales with
+    the values of the data. We don't want Heteroskedasticity.
 3. Independent Error Terms
     - Autocorrelation - a snaking plot - is bad.
 4. Normal errors
@@ -23,45 +25,6 @@ Dangers: Coefficients can be biased.  Standard errors could be unreliable (then 
     - Need to have truly independent x terms.
 6. Exogeneity
     - Omitted variable bias.
-"""
-"""
-Detailed 1:
-Residuals show the error in each observation relative to the fitted regression.
-We want to see a nice even spread.  y = b0 + b1(age)i + b2(age^2)i + e
-This is still a linear regression, because the inputs are linear wrt y.
-DO: residual plots, fit a linear regression to squared relation, and don't account for it.
-Estimated coefficients are not the best possible.
-
-Detailed 2:
-The spread in residuals increases as x increases (variance increases with x) - bad.
-Standard errors cannot be relied upon, but fitted result is still reasonably accurate.
-You can try to fix this by logging both variables.
-
-Detailed 3:
-This can only occur with ordered/time-series data. Wiggly residuals. Each residual is affected by the one before it.
-Estimated coefficients are still unbiased, but standard errors in output cannot be relied upon.
-Remedies: Investigate omitted variables (ie a business cycle for a stock index); generalised difference equation.
-
-Detailed 4:
-If there are a ton of zeros and a few other points, the standard errors are affected (if n is small).
-The true relationship will come out when there are enough examples. Small number of obs, standard errors are affected.
-Detect using a Q-Q plot. (If obs are on a sttraight diagonal line, it is normally-distributed.)
-To fix: change functional form (log?) and it's not a big deal just get more obs.
-
-Detailed 5:
-Number of cars and number of residents in development are pretty related (multi-collinearity).
-When we interpret each coefficient in a linear regression, we are "holding all other variables constant".
-If this isn't possible, then the model can't tell which of these actually made the difference - can't separate the effects.
-Coefficients and standard errors of affected variables are unreliable. To find: look at correlation (p) btwn X variables.
-Variance Inflation Factor (VIF) shows how much that variable's information is hidden in other, existing variables in model.
-Solution: remove one of variables. DO NOT multiply the variables together to fix this.
-
-Detailed 6:
-Omitted variable bias - a variable affects both X and Y, but is not included in the model.
-Socio-economic status would affect error term ei in the model. Thus, education is no longer wholly exogenous as it can be explained
-in part by the error term. Error term is not fully random. Children of professors vs children of janitors - this likely impacts
-years of education and salary. Result: Model can only be used for predictive purposes (can not infer causation). Can still get a
-valid prediction, but I can't say it's that 13th year of education that gave me this amount of higher salary.
 """
 
 def fit_linreg(x: np.ndarray, y: np.ndarray):
@@ -89,6 +52,10 @@ def fit_linreg(x: np.ndarray, y: np.ndarray):
 
 
 def single_regression(x,y):
+    """
+    Fits a linear regression to y using a single predictive 
+    x variable as input.
+    """
     st.write(f"## Linear Regression")
     st.write('GOAL: Predict sale price of house.')
     m,b,yhat,R2 = fit_linreg(x,y)
@@ -117,6 +84,9 @@ def single_regression(x,y):
 
 
 def multi_regression(x_mult,y):
+    """
+    Fits a linear regression to y using multiple x variables as input.
+    """
     st.write(f'## Multi-Linear Regression')
     st.write('GOAL: Predict sale price of house.')
     st.write(f'### Fitted Regression:')
@@ -153,7 +123,9 @@ def multi_regression(x_mult,y):
 
 
 def run_vif(x: pd.DataFrame):
-    from statsmodels.stats.outliers_influence import variance_inflation_factor
+    """
+    Calculate the correlation and VIF for a set of x variables.
+    """
     st.write(f"### Feature Relationships:")
     # VIF dataframe
     vif_data = pd.DataFrame()
@@ -210,3 +182,45 @@ if len(chosen_data.columns) > 1:
 elif len(chosen_data.columns) == 1:
     x = x.to_numpy()[:,0]
     single_regression(x,y)
+
+
+st.write(f"### Additional Information")
+"""
+Detailed 1:
+Residuals show the error in each observation relative to the fitted regression.
+We want to see a nice even spread.  y = b0 + b1(age)i + b2(age^2)i + e
+This is still a linear regression, because the inputs are linear wrt y.
+DO: residual plots, fit a linear regression to squared relation, and don't account for it.
+Estimated coefficients are not the best possible.
+
+Detailed 2:
+The spread in residuals increases as x increases (variance increases with x) - bad.
+Standard errors cannot be relied upon, but fitted result is still reasonably accurate.
+You can try to fix this by logging both variables.
+
+Detailed 3:
+This can only occur with ordered/time-series data. Wiggly residuals. Each residual is affected by the one before it.
+Estimated coefficients are still unbiased, but standard errors in output cannot be relied upon.
+Remedies: Investigate omitted variables (ie a business cycle for a stock index); generalised difference equation.
+
+Detailed 4:
+If there are a ton of zeros and a few other points, the standard errors are affected (if n is small).
+The true relationship will come out when there are enough examples. Small number of obs, standard errors are affected.
+Detect using a Q-Q plot. (If obs are on a sttraight diagonal line, it is normally-distributed.)
+To fix: change functional form (log?) and it's not a big deal just get more obs.
+
+Detailed 5:
+Number of cars and number of residents in development are pretty related (multi-collinearity).
+When we interpret each coefficient in a linear regression, we are "holding all other variables constant".
+If this isn't possible, then the model can't tell which of these actually made the difference - can't separate the effects.
+Coefficients and standard errors of affected variables are unreliable. To find: look at correlation (p) btwn X variables.
+Variance Inflation Factor (VIF) shows how much that variable's information is hidden in other, existing variables in model.
+Solution: remove one of variables. DO NOT multiply the variables together to fix this.
+
+Detailed 6:
+Omitted variable bias - a variable affects both X and Y, but is not included in the model.
+Socio-economic status would affect error term ei in the model. Thus, education is no longer wholly exogenous as it can be explained
+in part by the error term. Error term is not fully random. Children of professors vs children of janitors - this likely impacts
+years of education and salary. Result: Model can only be used for predictive purposes (can not infer causation). Can still get a
+valid prediction, but I can't say it's that 13th year of education that gave me this amount of higher salary.
+"""
